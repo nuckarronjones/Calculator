7be118324bb1d3e406c3120f5a4caa6218121de3
nick jones <njonesgrad@gmail.com>
2020-07-12 23:11:40 -0500

    Button/Cosmetics

--- Calculator/js.js
+++ Calculator/js.js
@@ -1,15 +1,17 @@
-
 let calculator = {
 	display: document.getElementById("number"),
-	operationToBePerformed: false,
-	xnum: "",//number to be operated on (prior to operation)
+	operationToBePerformed: "no",
+	//yes - operation is pending completion
+	//no - operation has not been applied to a number
+	//completed - equals sign was pressed. If not operations are utilized, screen will clear to show new digits
+	xnum: "0",//number to be operated on (prior to operation)
 	operation: "",//operation to be applied to xnum
 	firstNumber :true,//changes initial 0 if first time calculating
 
 	reset: function(){//reset screen values back to original config
 		//this.operationToBePerformed = false
-		this.operationToBePerformed = false
-		this.xnum = ""
+		this.operationToBePerformed = "no"
+		this.xnum = "0"
 		this.operation = ""
 		this.firstNumber = true
 	},
@@ -22,33 +24,76 @@ let calculator = {
 		console.log(operation + " : operation")
 		console.log(y+ " : Y")
 		calculator.display.innerHTML = `${answer}`
-		calculator.xnum = `${answer}`
+		calculator.xnum = `${answer}`//the start is now the end result. Can be operated on
 		console.log(answer + " ANSWER")
 	}
 }
 
+function formatNegative(value){
+	if(value.indexOf('-') == -1){//value is not a negative
+		console.log(1)
+		calculator.display.innerHTML = `-${value}`
+	}
+	else{//value is a negative
+		calculator.display.innerHTML = `${value}`.substring(1)//the value being fed back has a negative. substring removed first '-'
+		console.log(2)
+	}
+}
+
+function numberAnimate(e,idName){
+  	//animation changes
+  	console.log(idName + " IDNAME")
+
+  	switch(idName){
+  		case "num":
+  			e.target.style.animationName = "greyBttn"
+  			break
+  		case "function":
+  			e.target.style.animationName = "lightGreyBttn"
+  			break
+  		case "equals":
+  		case "operations":
+  			e.target.style.animationName = "orangeBttn"
+  			break
+  	}
+
+	e.target.style.animationDuration = "1.2s"
+	e.target.style.animationFillMode = "forwards"
+
+	setTimeout(()=>{//clear animation styles so button can be pressed again with animation
+		e.target.style.animationName = ""
+		e.target.style.animationDuration = ""
+		e.target.style.animationFillMode = ""
+	},1201)//additional second to let animation finish out
+}
+
 
 document.addEventListener("click",(e)=>{//listens for a button press
-	//console.log(e.target.id)//debug
 
 	let click = e.target.id
 	let value = e.target.innerHTML
 
+	numberAnimate(e,click)
+
 	switch(click){
 		case "num":
+		// animation-name: example;
+  		//animation-duration: 4s;
 
-			if(calculator.firstNumber){//checks for removal of starter 0
+			if(calculator.firstNumber){//checks for removal of starter 0, when someone first opens webapp/reloads. Changes initial 0 value
 				calculator.wipeScreen()
 				calculator.firstNumber = false
 			}
-			if(calculator.operationToBePerformed){
+			if(calculator.operationToBePerformed == "yes" || calculator.operationToBePerformed == "completed"){
+				console.log("operation to be performed")
 				calculator.wipeScreen()
 			}
+
 			calculator.display.innerHTML += e.target.innerHTML
 
 			//console.log(calculator.display.innerHTML.length)
 
-		break
+			break
 
 		case "function":
 			if(value == "AC"){
@@ -59,20 +104,19 @@ document.addEventListener("click",(e)=>{//listens for a button press
 			}
 			else if(value == "+/-"){
 				console.log("+/- chosen")//debug
+				formatNegative(calculator.display.innerHTML)
+				//calculator.display.innerHTML += "-"
 			}
 			else if(value == "%"){
 				console.log("% chosen")//debug
 			}
 
-		break
+			break
 
 		case "operations":
-			calculator.xnum = calculator.display.innerHTML//set xnum
 
-			//screen.operationToBePerformed = true
-
-			calculator.operationToBePerformed = true
-
+			calculator.operationToBePerformed = "yes"
+			
 			calculator.xnum = calculator.display.innerHTML
 
 			if(value == "÷"){
@@ -91,13 +135,17 @@ document.addEventListener("click",(e)=>{//listens for a button press
 				//console.log("+ Chosen")//debug
 				calculator.operation = "+"
 			}
-			else if(value == "="){
-				//console.log("= chosen")//debug
-				//console.log("TEST " + calculator.display.innerHTML)
-				console.log(calculator)
-				calculator.evaluate(calculator.xnum,calculator.operation,calculator.display.innerHTML)
-			}
 
-		break
+			break
+
+		case "equals":
+
+			console.log(calculator)
+
+			calculator.operationToBePerformed = "completed"
+
+			calculator.evaluate(calculator.xnum,calculator.operation,calculator.display.innerHTML)
+
+			break
 	}
 })
