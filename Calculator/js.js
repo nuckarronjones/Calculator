b108700b0bcb1e72ab87b1bcfaa80b1c7abd7e4a
Nicholas jones <scubaa4living@gmail.com>
2020-07-17 19:28:05 -0500

    Finished Functionality
    Things to fix/add
    -% button
    -Text resizes once it goes past screen
    -etc

--- Calculator/js.js
+++ Calculator/js.js
@@ -1,9 +1,6 @@
 let calculator = {
 	display: document.getElementById("number"),
 	operationToBePerformed: "no",
-	//yes - operation is pending completion
-	//no - operation has not been applied to a number
-	//completed - equals sign was pressed. If not operations are utilized, screen will clear to show new digits
 	xnum: "0",//number to be operated on (prior to operation)
 	operation: "",//operation to be applied to xnum
 	firstNumber :true,//changes initial 0 if first time calculating
@@ -29,57 +26,56 @@ let calculator = {
 	}
 }
 
+alert(calculator.display.innerHTML.length)
+
 function formatNegative(value){
 	if(value.indexOf('-') == -1){//value is not a negative
-		console.log(1)
+
 		calculator.display.innerHTML = `-${value}`
 	}
 	else{//value is a negative
+		
 		calculator.display.innerHTML = `${value}`.substring(1)//the value being fed back has a negative. substring removed first '-'
-		console.log(2)
 	}
 }
 
-function buttonAnimate(e,idName){
+function buttonAnimate(e,classVals){
+	if(classVals.contains("num")){
+		
+		e.target.style.animationName = "greyBttn"
 
-	//document.getElementById("operations").classList.add("shit")
+	}else if(classVals.contains("function")){
+		
+		e.target.style.animationName = "lightGreyBttn"
 
-	// alert(e.target.classList.contains("lightGreyBttn"))
-	// alert(e.target.classList)
+	}else if(classVals.contains("equals") || classVals.contains("operations")){
+			try{//find id=active, and remove all styles previously applied. If no previous id with "active" status exists, then catch the error and do nothing
+				//only one button must be active and animation played at one time
+				document.getElementById("active").removeAttribute("style")
+				document.getElementById("active").id = ""
+			}catch(e){}
 
-  	switch(idName){
-  		case "num":
-  			e.target.style.animationName = "greyBttn"
-  			break
-  		case "function":
-  			e.target.style.animationName = "lightGreyBttn"
-  			break
-  		case "equals":
-  		case "operations":
-  			e.target.style.animationFillMode = "forwards"
-  			e.target.style.animationName = "orangeBttn"
-  			e.target.style.color = "#FF9500"
-  			e.target.classList.add('active')
-  			break
-  	}
+			e.target.id = "active"
+			e.target.style.animationFillMode = "forwards"
+	 	 	e.target.style.animationName = "orangeBttn"
+	 	 	e.target.style.color = "#FF9500"
 
-	if(idName == "greyBttn" || idName == "lightGreyBttn"){//only reset non operations/equal button animations
+	}
+
+	if(classVals.contains("greyBttn") || classVals.contains("lightGreyBttn")){//only reset non operations/equal button animations
+		
 		setTimeout(()=>{//clear animation styles so button can be pressed again with animation
 			e.target.style.animationName = ""
-			e.target.style.animationDuration = ""
-			e.target.style.animationFillMode = ""
 		},1201)//additional second to let animation finish out
 	}
 }
 
 
-document.addEventListener("click",(e)=>{//listens for a button press
-
-	//let click = e.target.id
+document.addEventListener("click",(e)=>{//listens for a button press. Only those with certian classes will execute animations/functionality
 	let value = e.target.innerHTML
 	let classVals = e.target.classList
 
-	buttonAnimate(e,click)
+	buttonAnimate(e,classVals)
 
 	if(classVals.contains("num")){
 
@@ -89,7 +85,7 @@ document.addEventListener("click",(e)=>{//listens for a button press
 			calculator.wipeScreen()
 			calculator.firstNumber = false
 		}
-		if(calculator.operationToBePerformed == "yes" || calculator.operationToBePerformed == "completed"){
+		if(calculator.operationToBePerformed == "yes"){
 			console.log("operation to be performed")
 			calculator.wipeScreen()
 		}
@@ -107,6 +103,11 @@ document.addEventListener("click",(e)=>{//listens for a button press
 			calculator.display.innerHTML = "0"
 			calculator.firstNumber = true
 			calculator.reset()
+
+			try{//if an id of active exists, remove it as AC resets the calculator
+				document.getElementById("active").removeAttribute("style")
+				document.getElementById("active").id = ""
+			}catch(e){}
 		}
 		else if(value == "+/-"){
 			console.log("+/- chosen")//debug
@@ -150,76 +151,7 @@ document.addEventListener("click",(e)=>{//listens for a button press
 
 		console.log(calculator)
 
-		calculator.operationToBePerformed = "completed"
+		//calculator.operationToBePerformed = "completed"
 		calculator.evaluate(calculator.xnum,calculator.operation,calculator.display.innerHTML)
 	}
-	// switch(click){
-	// 	case "num":
-
-	// 		if(calculator.firstNumber){//checks for removal of starter 0, when someone first opens webapp/reloads. Changes initial 0 value
-	// 			calculator.wipeScreen()
-	// 			calculator.firstNumber = false
-	// 		}
-	// 		if(calculator.operationToBePerformed == "yes" || calculator.operationToBePerformed == "completed"){
-	// 			console.log("operation to be performed")
-	// 			calculator.wipeScreen()
-	// 		}
-
-	// 		calculator.display.innerHTML += e.target.innerHTML
-
-	// 		break
-
-	// 	case "function":
-	// 		if(value == "AC"){
-	// 			console.log("AC Chosen")//debug
-	// 			calculator.display.innerHTML = "0"
-	// 			calculator.firstNumber = true
-	// 			calculator.reset()
-	// 		}
-	// 		else if(value == "+/-"){
-	// 			console.log("+/- chosen")//debug
-	// 			formatNegative(calculator.display.innerHTML)
-	// 			//calculator.display.innerHTML += "-"
-	// 		}
-	// 		else if(value == "%"){
-	// 			console.log("% chosen")//debug
-	// 		}
-
-	// 		break
-
-	// 	case "operations":
-
-	// 		calculator.operationToBePerformed = "yes"
-			
-	// 		calculator.xnum = calculator.display.innerHTML
-
-	// 		if(value == "÷"){
-	// 			//console.log("percent Chosen")//debug
-	// 			calculator.operation = "/"
-	// 		}
-	// 		else if(value == "×"){
-	// 			//console.log("times chosen")//debug
-	// 			calculator.operation = "*"
-	// 		}
-	// 		else if(value == "-"){
-	// 			//console.log("- chosen")//debug
-	// 			calculator.operation = "-"
-	// 		}
-	// 		else if(value == "+"){
-	// 			//console.log("+ Chosen")//debug
-	// 			calculator.operation = "+"
-	// 		}
-
-	// 		break
-
-	// 	case "equals":
-
-	// 		console.log(calculator)
-
-	// 		calculator.operationToBePerformed = "completed"
-
-	// 		calculator.evaluate(calculator.xnum,calculator.operation,calculator.display.innerHTML)
-
-	// 		break
-	// }
 })
